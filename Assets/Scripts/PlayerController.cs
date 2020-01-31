ff2cc43e5a23ac307af67def077575c4c5685ac9
Eddie <eddie.rayner1@gmail.com>
2020-01-31 21:14:50 +0000

    Unity initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Vector2 kbDirection;
+    Rigidbody2D playerRB;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# GamJam2020
+Sussex uni game jam 2020 (theme - repair)
