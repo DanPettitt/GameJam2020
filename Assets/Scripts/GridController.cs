dee5ff374f17414fdb57158703b53941e673fd8a
Eddie <eddie.rayner1@gmail.com>
2020-01-31 22:29:18 +0000

    Day 1 basic scripts

---
+++ Assets/Scripts/GridController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridController : MonoBehaviour
+{
+    public int WIDTH;
+    public int HEIGHT;
+
+    GameObject[,] grid;
+
+    private void Start()
+    {
+        grid = new GameObject[WIDTH,HEIGHT];
+        for (int x = 0; x < WIDTH; x++)
+        {
+            for (int y = 0; y < HEIGHT; y++)
+            {
+
+                Collider2D collider = Physics2D.OverlapBox(new Vector2(x, y), new Vector2(0.5f, 0.5f), 0);
+                if (collider != null && !collider.CompareTag("Player"))
+                {
+                    grid[x, y] = collider.gameObject;
+                    Debug.Log(x);
+                    Debug.Log(y);
+                }
+            }
+        }
+    }
+
+    public GameObject GetContents(int x, int y)
+    {
+        return grid[x, y];
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,39 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Vector2 kbDirection;
-    Rigidbody2D playerRB;
+    Vector2 keyboardDirection;
+    Vector2 facingDirection;
+    Rigidbody2D playerRigidbody;
+    GridController gridController;
+
+    public int MOVEMENTSPEED = 5;
+
+    private void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+        gridController = GameObject.Find("Grid").GetComponent<GridController>();
+        
+    }
+
+    private void FixedUpdate()
+    {
+
+        // Move the player based on keyboard input
+        playerRigidbody.velocity = keyboardDirection * MOVEMENTSPEED;
+
+        // Get the direction the player is facing in case the player stops the last direction will be saved
+        if (playerRigidbody.velocity != Vector2.zero)
+        {
+            facingDirection = playerRigidbody.velocity.normalized;
+        }
+
+
+    }
+
+    private void Update()
+    {
+        // Get input from the keyboard to see the direction to move the player
+        keyboardDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+    }
+
 }
